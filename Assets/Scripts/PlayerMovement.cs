c0d0fab7b8efa3d71a670e65806d35bc0b768e9d
mariana maysa <marianamaysaestudos@gmail.com>
2023-11-25 16:05:38 -0300

    Move basic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+Ôªøusing System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
@@ -20,7 +20,7 @@ public class PlayerMovement : NetworkBehaviour
         }
         else
         {
-            vcam.Priority = 0;   
+            vcam.Priority = 0;
         }
     }
 
@@ -45,7 +45,7 @@ public class PlayerMovement : NetworkBehaviour
         controller = GetComponent<CharacterController>();
     }
 
-   
+
     void Update()
     {
 
@@ -79,7 +79,7 @@ public class PlayerMovement : NetworkBehaviour
         direction.y = jumpVelocity;
         controller.Move(direction * Time.deltaTime);
 
-        //reunir as informaÁıes sobre em qual pista devemos estar
+        //reunir as informa√ß√µes sobre em qual pista devemos estar
 
         if (Input.GetKeyDown(KeyCode.A))
         {
@@ -91,79 +91,33 @@ public class PlayerMovement : NetworkBehaviour
         }
 
         else if (Input.GetKeyDown(KeyCode.D))
+        {
+            desiredLane++;
+            if (desiredLane > 2)
             {
-                desiredLane++;
-                if (desiredLane > 2)
-                {
-                    desiredLane = 0;
-                }
+                desiredLane = 0;
             }
+        }
 
         //calcular onde devemos estar no futuro
-        
+
         Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
 
-        if(isNearWall() && wallRight && desiredLane == 2)
+        if (desiredLane == 0)
         {
             targetPosition += Vector3.left * laneDistance;
         }
-        else if(isNearWall() && !wallRight && desiredLane == 0)
+        else if (desiredLane == 1)
+        {
+
+        }
+        else if (desiredLane == 2)
         {
             targetPosition += Vector3.right * laneDistance;
-        }
-        else
-        {
-            if (desiredLane == 0)
-            {
-                targetPosition += Vector3.left * laneDistance;
-            }
-            else if (desiredLane == 1)
-            {
-
-            }
-            else if (desiredLane == 2)
-            {
-                targetPosition += Vector3.right * laneDistance;
-            }
         }
 
         transform.position = Vector3.Lerp(transform.position, targetPosition, 70 * Time.deltaTime);
 
-        //detectar input do wallrunning
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            if (Input.GetKey(KeyCode.A))
-            {
-                StartWallRun(-1); //esquerda
-            }
-            else if (Input.GetKey(KeyCode.D))
-            {
-                StartWallRun(1); //direita
-            }
-        }       
-    }
-
-    void StartWallRun(int direction)
-    {
-        if (isNearWall())
-        {
-            Vector3 wallRunDirection = orientation.right * direction;
-            controller.Move(wallRunDirection * slideSpeed * Time.deltaTime);
-        }
-    }
-
-    bool isNearWall()
-    {
-        RaycastHit wallHit;
-
-        //verificar se tem parede na direÁ„o do personagem
-        if (Physics.Raycast(transform.position, orientation.forward, out wallHit, wallCheckDistance, whatIsWall))
-        {
-            return true;
-        }
-
-        return false;
     }
 
     void Move(Vector3 direction)
@@ -180,7 +134,7 @@ public class PlayerMovement : NetworkBehaviour
     {
         jumpVelocity -= gravity;
     }
-    
+
     void StartSlide()
     {
         StartCoroutine(SlideRoutine());
@@ -197,7 +151,7 @@ public class PlayerMovement : NetworkBehaviour
     {
         Vector3 slideDirection = Vector3.zero;
 
-        //verificar se tem colis„o com as paredes
+        //verificar se tem colis√£o com as paredes
 
         if (controller.isGrounded && (controller.collisionFlags & CollisionFlags.Sides) != 0)
         {
@@ -209,11 +163,12 @@ public class PlayerMovement : NetworkBehaviour
 
     bool isCollidingWithRightWall()
     {
-        // Implemente a lÛgica para verificar se o personagem est· colidindo com a parede esquerda
+        // Implemente a l√≥gica para verificar se o personagem est√° colidindo com a parede esquerda
         wallRight = Physics.Raycast(transform.position, orientation.right, out rightWallHit, wallCheckDistance, whatIsWall);
 
-        return true;
+        return false;
 
-        // Use Raycast ou outras tÈcnicas de detecÁ„o de colis„o
+        // Use Raycast ou outras t√©cnicas de detec√ß√£o de colis√£o
     }
 }
+
